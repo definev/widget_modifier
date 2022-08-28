e613833fea1faea0088eb9d838801f2d28937800
daiduong.workmail <daiduong.workmail@gmail.com>
2022-08-29 01:43:01 +0700

    feat: add `layout_builder` modifier

--- lib/src/widgets/layout_builder.dart
+++ lib/src/widgets/layout_builder.dart
@@ -1,1 +1,25 @@
-// TODO(definev): Implement this library.
+import 'package:flutter/widgets.dart';
+import 'package:nested/nested.dart';
+
+typedef SingleChildLayoutWidgetBuilder = Widget Function(
+    BuildContext context, BoxConstraints constraints, Widget? child);
+
+class LayoutBuilderModifier extends SingleChildStatelessWidget {
+  const LayoutBuilderModifier({
+    Key? key,
+    Widget? child,
+    this.modifierKey,
+    required this.builder,
+  }) : super(key: key, child: child);
+
+  final Key? modifierKey;
+  final SingleChildLayoutWidgetBuilder builder;
+
+  @override
+  Widget buildWithChild(BuildContext context, Widget? child) {
+    return LayoutBuilder(
+      key: modifierKey,
+      builder: (context, constraints) => builder(context, constraints, child),
+    );
+  }
+}
